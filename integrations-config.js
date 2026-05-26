7314ecd9ed06670796e001b5fe903a3f538f3b08
emrsnjr232026-tech <emrsnjr23.2026@gmail.com>
2026-05-25 21:38:12 -0300

    Update integrations-config.js

--- integrations-config.js
+++ integrations-config.js
@@ -1,4 +1,4 @@
 // Cole aqui a URL do Google Apps Script publicado como "app da Web".
 // Essa URL centraliza o cardapio da semana e as comandas para todos os clientes.
 // Se deixar vazio, o site funciona apenas com o banco local deste navegador.
-export const googleSheetsWebhookUrl = "https://script.google.com/macros/s/AKfycbwAB5nJBA0nlu7g6sQH7WIA6u4ogglNfvuKoIYJJPVuHqovhreCGInWI_FGUPTfZv4n/exec";
+export const googleSheetsWebhookUrl = "https://script.google.com/macros/s/AKfycbxNmkO5Ndt1QBSqGCDUQdJ_MmUmSHEdLS7GK6kOuOuZ7MY0cfhkKWPa9a074iHzjOeG/exec";
